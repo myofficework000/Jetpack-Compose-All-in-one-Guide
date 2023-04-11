36815773428c37b48828ae985c8c5fc2bacb497f
Luan Dang <luandang9697@gmail.com>
2023-04-11 14:50:50 -0500

    added Delete icon button and implemented delete feature

--- app/src/main/java/com/example/jetpack_compose_all_in_one/ui/theme/Dimens.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/ui/theme/Dimens.kt
@@ -14,13 +14,16 @@ val spaceExtraLarge: Dp = 64.dp
 val spaceXXLarge: Dp = 128.dp
 val spaceXXXLarge: Dp = 256.dp
 
-val dp_0: Dp = 0.dp
-val dp_1: Dp = 1.dp
-val dp_2: Dp = 2.dp
-val dp_3: Dp = 3.dp
-val dp_10: Dp = 10.dp
 
 val sp_10 = 10.sp
 val sp_12 = 12.sp
 val sp_14 = 14.sp
 val sp_16 = 16.sp
+val dp_0: Dp = 0.dp
+val dp_1: Dp = 1.dp
+val dp_2: Dp = 2.dp
+val dp_10: Dp = 10.dp
+val dp_3: Dp = 3.dp
+val dp_4: Dp = 4.dp
+val dp_24: Dp = 24.dp
+
