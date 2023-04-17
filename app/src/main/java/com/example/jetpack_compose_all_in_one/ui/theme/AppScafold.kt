52a63d93b6492f65a07595c0f3d8781e92ccbdb8
1+1=2 <hau.chan@jessup.edu>
2023-04-17 13:35:59 -0700

    Music for Bound Service
    Other than starting and stopping, it can also pause for 10s.
    Everything stops if the app is closed or tabbed out.

--- app/src/main/java/com/example/jetpack_compose_all_in_one/ui/theme/AppScafold.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/ui/theme/AppScafold.kt
@@ -2,30 +2,42 @@ package com.example.jetpack_compose_all_in_one.ui.theme
 
 import android.media.RingtoneManager
 import android.net.Uri
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import com.example.jetpack_compose_all_in_one.BottomNavBar
+import androidx.compose.ui.unit.dp
+import com.example.jetpack_compose_all_in_one.*
 import com.example.jetpack_compose_all_in_one.R
-import com.example.jetpack_compose_all_in_one.SimpleIconButton
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainContainerOfApp(
     isOffline: Boolean,
-    playMusicFunc: (Uri) -> Unit,
-    stopMusicFunc: () -> Unit
+    playMusicFuncForeground: (Uri) -> Unit,
+    stopMusicFuncForeground: () -> Unit,
+    playMusicFuncBound: (Uri) -> Unit,
+    stopMusicFuncBound: () -> Unit,
+    pauseMusicFuncBound: (Long) -> Unit,
+    resumeMusicFuncBound: () -> Unit
 ) {
 
     Scaffold(
         topBar = {
             TopAppBar(title = { Text(stringResource(id = R.string.app_name)) }, actions = {
                 SimpleIconButton(R.drawable.baseline_play_circle_outline_24) {
-                    playMusicFunc(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE))
+                    playMusicFuncForeground(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE))
                 }
                 SimpleIconButton(R.drawable.outline_stop_circle_24) {
-                    stopMusicFunc()
+                    stopMusicFuncForeground()
                 }
             })
         },
@@ -37,6 +49,22 @@ fun MainContainerOfApp(
            NetworkErrorDialog()
         } else {
             Text("Internet available")
+        }
+        Box(
+            Modifier.padding(it).fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                Modifier
+                    .background(Color(0.314f, 0.29f, 0.29f, 0.725f))
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                SimpleTextButton("Start") { playMusicFuncBound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)) }
+                SimpleTextButton("Stop") { stopMusicFuncBound() }
+                SimpleTextButton("Pause (10s)") { pauseMusicFuncBound(10000L) }
+                SimpleTextButton("Resume") { resumeMusicFuncBound() }
+            }
         }
     }
 }
