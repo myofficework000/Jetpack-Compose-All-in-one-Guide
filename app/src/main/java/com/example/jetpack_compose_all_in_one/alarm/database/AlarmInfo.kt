73d1e4247a079de08123637a89a7d774c5f4da95
1+1=2 <hau.chan@jessup.edu>
2023-04-19 08:55:53 -0700

    Update AlarmInfo.kt

--- app/src/main/java/com/example/jetpack_compose_all_in_one/alarm/database/AlarmInfo.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/alarm/database/AlarmInfo.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "AlarmInfo")
 data class AlarmInfo(
-    @PrimaryKey val reqCode: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val hour: Int,
     val minute: Int,
     val enabled: Boolean
