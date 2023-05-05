82ed52d8d6ef17872c2e83ee319ec4d65cc5a74b
cheetahmail007 <cheetahmail007@gmail.com>
2023-05-06 00:43:54 +0530

    added simple dog UI blank

---
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/features/dog_api/view/RandomDogUI.kt
@@ -0,0 +1,13 @@
+package com.example.jetpack_compose_all_in_one.features.dog_api.view
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun RandomDogUI() {
+
+    //Dog viewmodel take request and show UI
+    //Column
+    // GlideImage or Coil Imageg
+    //Button
+}
