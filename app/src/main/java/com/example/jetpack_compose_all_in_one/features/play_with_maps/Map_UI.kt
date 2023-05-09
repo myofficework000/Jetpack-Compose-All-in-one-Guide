c86d60437f80b44119c6fd65980ce7e531910019
1+1=2 <hau.chan@jessup.edu>
2023-05-09 12:00:08 -0700

    Play with Maps, yet again...
    - Allowed getting current position, updates every 10 seconds.

--- app/src/main/java/com/example/jetpack_compose_all_in_one/features/play_with_maps/Map_UI.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/features/play_with_maps/Map_UI.kt
@@ -1,5 +1,6 @@
 package com.example.jetpack_compose_all_in_one.features.play_with_maps
 
+import android.location.Location
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,8 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -17,19 +23,22 @@ import com.example.jetpack_compose_all_in_one.R
 import com.example.jetpack_compose_all_in_one.ui.components.SimpleTextButton
 import com.example.jetpack_compose_all_in_one.ui.theme.dp_100
 import com.example.jetpack_compose_all_in_one.ui.theme.dp_50
+import com.example.jetpack_compose_all_in_one.utils.Constants.LOCATION_UPDATE_INTERVAL
 import com.example.jetpack_compose_all_in_one.utils.Constants.MAP_POS_BIGBEN
 import com.example.jetpack_compose_all_in_one.utils.bitmapDescriptorFromRes
-import com.google.android.gms.maps.GoogleMap
+import com.example.jetpack_compose_all_in_one.utils.requestAllLocation
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.Circle
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
-import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.Polygon
 import com.google.maps.android.compose.Polyline
+import kotlinx.coroutines.delay
 
 // Step 1- verify google map
 //step 2-  add marker
@@ -38,12 +47,43 @@ import com.google.maps.android.compose.Polyline
 //step 5- add polygon
 // step 6 - add polyline
 // step 7 - add current location feature
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun ComposeDemoApp(
-    vm: MapViewModel
+    vm: MapViewModel,
+    getCurrentLocationFunc: ((Location?) -> Unit) -> Unit
 ) {
     val ctx = LocalContext.current
     val willDraw = rememberSaveable { mutableStateOf(false) }
+
+    var isLocationAvailable by remember{ mutableStateOf(false) }
+    var requestingLocation = requestAllLocation{ isLocationAvailable = it }
+    var currentLocation by remember { mutableStateOf<Location?>(null) }
+    val currentLocationMarker = remember{ derivedStateOf { MarkerState(
+        if (currentLocation == null) MAP_POS_BIGBEN
+        else LatLng(currentLocation!!.latitude, currentLocation!!.longitude)
+    ) } }
+
+    LaunchedEffect(isLocationAvailable) {
+        while (isLocationAvailable) {
+            getCurrentLocationFunc{
+                currentLocation = it
+
+                val state = vm.cameraPositionState
+                it?.let { loc ->
+                    state.move(CameraUpdateFactory.newCameraPosition(
+                        CameraPosition(
+                            LatLng(loc.latitude, loc.longitude),
+                            state.position.zoom,
+                            state.position.tilt,
+                            state.position.bearing
+                        )
+                    ))
+                }
+            }
+            delay(LOCATION_UPDATE_INTERVAL)
+        }
+    }
 
     Column(
         Modifier.fillMaxSize()
@@ -51,7 +91,8 @@ fun ComposeDemoApp(
         Column(
             Modifier
                 .fillMaxWidth()
-                .padding(16.dp)) {
+                .padding(16.dp)
+        ) {
             Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
                 SimpleTextButton(
                     if (willDraw.value) "Ew hide this thing" else "Draw something"
@@ -62,25 +103,30 @@ fun ComposeDemoApp(
                 SimpleTextButton("Mode: ${vm.mapTypeDisplayName}") { vm.changeMapType() }
             }
 
-            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-                SimpleTextButton("Go to current position", enabled = false) {
-                    // W.I.P.
+            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly){
+                SimpleTextButton("Go to current position", enabled = !isLocationAvailable) {
+                    requestingLocation.launchMultiplePermissionRequest()
                 }
 
                 SimpleTextButton("Nothing", enabled = false) {
                     // W.I.P.
                 }
-
             }
         }
 
-        GoogleMapDynamic(
+        GoogleMap(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
             cameraPositionState = vm.cameraPositionState,
-            mapType = vm.mapType.value
+            properties = MapProperties(mapType = vm.mapType2.value, isMyLocationEnabled = isLocationAvailable)
         ) {
+            Marker(
+                state = currentLocationMarker.value,
+                title = "Look at you",
+                snippet = "What."
+            )
+
             if (willDraw.value) {
                 Marker(
                     state = MarkerState(position = MAP_POS_BIGBEN),
@@ -125,6 +171,7 @@ fun ComposeDemoApp(
     }
 }
 
+/*
 @Composable
 private fun GoogleMapDynamic(
     modifier: Modifier = Modifier,
@@ -178,5 +225,4 @@ private fun GoogleMapDynamic(
             )
         }
     }
-
-}
+}*/
