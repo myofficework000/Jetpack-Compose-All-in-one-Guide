1cdb32e4605916e51876ffe2f2d0c8aa4678653e
cheetahmail007 <cheetahmail007@gmail.com>
2023-05-11 00:44:39 +0530

    minor changes in code review

--- app/src/main/java/com/example/jetpack_compose_all_in_one/lessons/lesson_5/Lesson_5_chapter_1_Map_Basic.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/lessons/lesson_5/Lesson_5_chapter_1_Map_Basic.kt
@@ -1,6 +1,5 @@
 package com.example.jetpack_compose_all_in_one.lessons.lesson_5
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,17 +10,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose_all_in_one.R
 import com.example.jetpack_compose_all_in_one.ui.components.LessonHeader
 import com.example.jetpack_compose_all_in_one.ui.theme.dp_100
+import com.example.jetpack_compose_all_in_one.ui.theme.dp_15
 import com.example.jetpack_compose_all_in_one.ui.theme.dp_50
 import com.example.jetpack_compose_all_in_one.utils.Constants
 import com.example.jetpack_compose_all_in_one.utils.LogicPager
@@ -38,7 +35,7 @@ import com.google.maps.android.compose.Polygon
 import com.google.maps.android.compose.Polyline
 import com.google.maps.android.compose.rememberCameraPositionState
 
-@Preview()
+@Preview
 @Composable
 fun Lesson_5_Chapter_Map_Basic() {
     LessonContent()
@@ -46,25 +43,24 @@ fun Lesson_5_Chapter_Map_Basic() {
 
 @Composable
 private fun LessonContent() {
-
-    val ctx = LocalContext.current
-
-    val cameraPositionState = rememberCameraPositionState{
+    val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(Constants.MAP_POS_BIGBEN, 18.54f)
     }
-    val currentPage = rememberSaveable{ mutableStateOf(0) }
-    val lessonHeaderText by remember{ derivedStateOf {
-        when (currentPage.value) {
-            1 -> "Inflate Marker On Map"
-            2 -> "Customize Marker On Map"
-            3 -> "Create PolyLine on Map"
-            4 -> "Create Polygon on Map"
-            5 -> "Create Circle on Map"
-            else -> "Simple Map"
+    val currentPage = rememberSaveable { mutableStateOf(0) }
+    val lessonHeaderText by remember {
+        derivedStateOf {
+            when (currentPage.value) {
+                1 -> "Inflate Marker On Map"
+                2 -> "Customize Marker On Map"
+                3 -> "Create PolyLine on Map"
+                4 -> "Create Polygon on Map"
+                5 -> "Create Circle on Map"
+                else -> "Simple Map"
+            }
         }
-    } }
+    }
 
-    var mapType by remember{ mutableStateOf(MapType.NORMAL) }
+    val mapType by remember { mutableStateOf(MapType.NORMAL) }
 
     LogicPager(
         pages = listOf(
@@ -84,87 +80,26 @@ private fun LessonContent() {
         ) {
             LessonHeader(
                 lessonHeaderText,
-                Modifier.fillMaxWidth().padding(16.dp),
+                Modifier
+                    .fillMaxWidth()
+                    .padding(dp_15),
                 TextAlign.Center
             )
 
             GoogleMap(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp),
+                    .padding(dp_15),
                 cameraPositionState = cameraPositionState,
                 properties = MapProperties(mapType = mapType)
             ) {
                 when (currentPage.value) {
                     0 -> {}
-                    1 -> {
-                        Marker(
-                            state = MarkerState(position = Constants.MAP_POS_BIGBEN),
-                            title = "Regular marker",
-                            snippet = "This is a regular marker."
-                        )
-                    }
-                    2 -> {
-                        Marker(
-                            state = MarkerState(position = Constants.MAP_POS_BIGBEN),
-                            title = "Custom Marker",
-                            snippet = "This is a marker with custom bitmap.",
-                            icon = bitmapDescriptorFromRes(ctx, R.drawable.cannon, dp_100, dp_50)
-                        )
-                    }
-                    3 -> {
-                        Polyline(
-                            points = listOf(
-                                LatLng(
-                                    Constants.MAP_POS_BIGBEN.latitude + 0.0002,
-                                    Constants.MAP_POS_BIGBEN.longitude + 0.0003
-                                ),
-                                LatLng(
-                                    Constants.MAP_POS_BIGBEN.latitude - 0.0002,
-                                    Constants.MAP_POS_BIGBEN.longitude - 0.0003
-                                )
-                            )
-                        )
-                        Polyline(
-                            points = listOf(
-                                LatLng(
-                                    Constants.MAP_POS_BIGBEN.latitude + 0.0002,
-                                    Constants.MAP_POS_BIGBEN.longitude - 0.0003
-                                ),
-                                LatLng(
-                                    Constants.MAP_POS_BIGBEN.latitude - 0.0002,
-                                    Constants.MAP_POS_BIGBEN.longitude + 0.0003
-                                )
-                            )
-                        )
-                    }
-                    4 -> {
-                        Polygon(
-                            points = listOf(
-                                LatLng(
-                                    Constants.MAP_POS_BIGBEN.latitude + 0.0002,
-                                    Constants.MAP_POS_BIGBEN.longitude
-                                ),
-                                LatLng(
-                                    Constants.MAP_POS_BIGBEN.latitude - 0.0002,
-                                    Constants.MAP_POS_BIGBEN.longitude - 0.0003
-                                ),
-                                LatLng(
-                                    Constants.MAP_POS_BIGBEN.latitude - 0.0002,
-                                    Constants.MAP_POS_BIGBEN.longitude + 0.0003
-                                )
-                            ),
-                            fillColor = Color.Transparent,
-                            strokeColor = Color.Black,
-                            zIndex = 1.0f
-                        )
-                    }
-                    5 -> {
-                        Circle(
-                            center = Constants.MAP_POS_BIGBEN,
-                            radius = 10.0
-                        )
-                    }
+                    1 -> InflateMarkerOnMap()
+                    2 -> CustomMarkerOnMap()
+                    3 -> CreatePolyLineOnMap()
+                    4 -> CreatePolygonOnMap()
+                    5 -> CreateCircleOnMap()
                 }
             }
         }
@@ -172,32 +107,80 @@ private fun LessonContent() {
 }
 
 @Composable
-fun MapSimpleUI() {
-
-}
-
-@Composable
 fun InflateMarkerOnMap() {
-
+    Marker(
+        state = MarkerState(position = Constants.MAP_POS_BIGBEN),
+        title = "Regular marker",
+        snippet = "This is a regular marker."
+    )
 }
 
 @Composable
 fun CustomMarkerOnMap() {
-
+    Marker(
+        state = MarkerState(position = Constants.MAP_POS_BIGBEN),
+        title = "Custom Marker",
+        snippet = "This is a marker with custom bitmap.",
+        icon = bitmapDescriptorFromRes(LocalContext.current, R.drawable.cannon, dp_100, dp_50)
+    )
 }
 
 
 @Composable
 fun CreatePolyLineOnMap() {
-
+    Polyline(
+        points = listOf(
+            LatLng(
+                Constants.MAP_POS_BIGBEN.latitude + 0.0002,
+                Constants.MAP_POS_BIGBEN.longitude + 0.0003
+            ),
+            LatLng(
+                Constants.MAP_POS_BIGBEN.latitude - 0.0002,
+                Constants.MAP_POS_BIGBEN.longitude - 0.0003
+            )
+        )
+    )
+    Polyline(
+        points = listOf(
+            LatLng(
+                Constants.MAP_POS_BIGBEN.latitude + 0.0002,
+                Constants.MAP_POS_BIGBEN.longitude - 0.0003
+            ),
+            LatLng(
+                Constants.MAP_POS_BIGBEN.latitude - 0.0002,
+                Constants.MAP_POS_BIGBEN.longitude + 0.0003
+            )
+        )
+    )
 }
 
 @Composable
 fun CreatePolygonOnMap() {
-
+    Polygon(
+        points = listOf(
+            LatLng(
+                Constants.MAP_POS_BIGBEN.latitude + 0.0002,
+                Constants.MAP_POS_BIGBEN.longitude
+            ),
+            LatLng(
+                Constants.MAP_POS_BIGBEN.latitude - 0.0002,
+                Constants.MAP_POS_BIGBEN.longitude - 0.0003
+            ),
+            LatLng(
+                Constants.MAP_POS_BIGBEN.latitude - 0.0002,
+                Constants.MAP_POS_BIGBEN.longitude + 0.0003
+            )
+        ),
+        fillColor = Color.Transparent,
+        strokeColor = Color.Black,
+        zIndex = 1.0f
+    )
 }
 
 @Composable
 fun CreateCircleOnMap() {
-
+    Circle(
+        center = Constants.MAP_POS_BIGBEN,
+        radius = 10.0
+    )
 }
--- app/src/main/java/com/example/jetpack_compose_all_in_one/utils/CommonUtils.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/utils/CommonUtils.kt
@@ -6,14 +6,10 @@ import android.graphics.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,14 +18,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
+import com.example.jetpack_compose_all_in_one.R
 import com.example.jetpack_compose_all_in_one.ui.components.SimpleTextButton
+import com.example.jetpack_compose_all_in_one.ui.theme.PAGER_BACKGROUND
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 
 fun bitmapDescriptorFromRes(
@@ -38,14 +36,14 @@ fun bitmapDescriptorFromRes(
     width: Dp? = null,
     height: Dp? = null
 ) = ContextCompat.getDrawable(context, redId)?.run {
-        val w = width?.value?.toInt() ?: intrinsicWidth
-        val h = height?.value?.toInt() ?: intrinsicHeight
+    val w = width?.value?.toInt() ?: intrinsicWidth
+    val h = height?.value?.toInt() ?: intrinsicHeight
 
-        setBounds(0, 0, w, h)
-        val bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-        draw(Canvas(bitmap))
-        BitmapDescriptorFactory.fromBitmap(bitmap)
-    }
+    setBounds(0, 0, w, h)
+    val bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+    draw(Canvas(bitmap))
+    BitmapDescriptorFactory.fromBitmap(bitmap)
+}
 
 // cleanupPerPage runs first, then cleanupAllPage
 @Composable
@@ -54,10 +52,10 @@ fun LogicPager(
     pages: List<() -> Unit>,
     cleanupPerPage: List<() -> Unit> = listOf(),
     cleanupAllPages: () -> Unit = {},
-    currentPage: MutableState<Int> = rememberSaveable{ mutableStateOf(0) },
+    currentPage: MutableState<Int> = rememberSaveable { mutableStateOf(0) },
     content: @Composable (PaddingValues) -> Unit
 ) {
-    val boxHeight = rememberSaveable{ mutableStateOf(0) }
+    val boxHeight = rememberSaveable { mutableStateOf(0) }
     val density = LocalDensity.current
 
     content(PaddingValues(top = boxHeight.value.dp))
@@ -65,7 +63,7 @@ fun LogicPager(
     if (pages.size > 1) {
         Box(
             modifier.then(Modifier.onGloballyPositioned {
-                with (density) { boxHeight.value = it.size.height.toDp().value.toInt() }
+                with(density) { boxHeight.value = it.size.height.toDp().value.toInt() }
             }),
             contentAlignment = Alignment.Center
         ) {
@@ -74,7 +72,10 @@ fun LogicPager(
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                SimpleTextButton("Prev", enabled = currentPage.value > 0) {
+                SimpleTextButton(
+                    stringResource(id = R.string.prev),
+                    enabled = currentPage.value > 0
+                ) {
                     if (currentPage.value < cleanupPerPage.size) cleanupPerPage[currentPage.value]()
                     cleanupAllPages()
                     currentPage.value--
@@ -87,8 +88,10 @@ fun LogicPager(
                     }
                 }
 
-
-                SimpleTextButton("Next", enabled = currentPage.value < pages.size - 1) {
+                SimpleTextButton(
+                    stringResource(id = R.string.next),
+                    enabled = currentPage.value < pages.size - 1
+                ) {
                     if (currentPage.value < cleanupPerPage.size) cleanupPerPage[currentPage.value]()
                     cleanupAllPages()
                     currentPage.value++
@@ -102,8 +105,8 @@ fun LogicPager(
 @Preview
 @Composable
 private fun LoginPagerPreview() {
-    // The regular preview doesn't show, but once it goes into interactive mode,
-    //      the page will get the padding.
+    //The regular preview doesn't show, but once it goes into interactive mode,
+    //the page will get the padding.
     LogicPager(
         Modifier.fillMaxWidth(),
         listOf(
@@ -112,6 +115,11 @@ private fun LoginPagerPreview() {
             {}
         )
     ) {
-        Box(Modifier.fillMaxSize().padding(it).background(Color.Yellow)) {}
+        Box(
+            Modifier
+                .fillMaxSize()
+                .padding(it)
+                .background(PAGER_BACKGROUND)
+        ) {}
     }
 }
