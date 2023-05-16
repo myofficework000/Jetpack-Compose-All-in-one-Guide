a1d83d9b4d67fda612af808d4c1b28d5d9490c2c
cheetahmail007 <cheetahmail007@gmail.com>
2023-05-16 20:36:25 +0530

    added dummy for progress UI

---
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/ui/components/ProgressUI.kt
@@ -0,0 +1,13 @@
+package com.example.jetpack_compose_all_in_one.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SimpleProgress() {
+    // Alex write your code here
+}
+
+@Composable
+fun CustomProgress() {
+
+}
