88b23abee600a0c5805e4b31d7d4fced2b5d4278
cheetahmail007 <cheetahmail007@gmail.com>
2023-05-18 04:49:58 +0530

    Code review

--- app/src/main/java/com/example/jetpack_compose_all_in_one/di/NotificationModule.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/di/NotificationModule.kt
@@ -3,7 +3,6 @@ package com.example.jetpack_compose_all_in_one.di
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.example.jetpack_compose_all_in_one.R
@@ -36,15 +35,12 @@ class NotificationModule {
         @ApplicationContext context: Context
     ):NotificationManagerCompat{
         val notificationManager = NotificationManagerCompat.from(context)
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            val channel = NotificationChannel(
-                "Main Channel ID",
-                "Main Channel",
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            notificationManager.createNotificationChannel(channel)
-        }
+        val channel = NotificationChannel(
+            "Main Channel ID",
+            "Main Channel",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(channel)
         return notificationManager
     }
-
 }
