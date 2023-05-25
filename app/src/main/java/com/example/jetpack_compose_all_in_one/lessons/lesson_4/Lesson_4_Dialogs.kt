14d4cdeaf03217f9fe8f045b4615910ef6b260cc
alex <mgpyaephyolay@gmail.com>
2023-05-25 16:01:39 -0500

    updated bottom sheet

---
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/lessons/lesson_4/Lesson_4_Dialogs.kt
@@ -0,0 +1,55 @@
+package com.example.jetpack_compose_all_in_one.lessons.lesson_4
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.text.style.TextAlign
+import com.example.jetpack_compose_all_in_one.R
+import com.example.jetpack_compose_all_in_one.ui.components.AlertDialog
+import com.example.jetpack_compose_all_in_one.ui.components.BottomSheet
+import com.example.jetpack_compose_all_in_one.ui.components.LessonHeader
+import com.example.jetpack_compose_all_in_one.ui.theme.dp_15
+import com.example.jetpack_compose_all_in_one.utils.LogicPager
+
+@Composable
+fun Lesson_4_Chapter_Dialogs() {
+    LessonContent()
+}
+
+@Composable
+private fun LessonContent() {
+    val currentPage = rememberSaveable { mutableStateOf(0) }
+
+    LogicPager(
+        pageCount = 4,
+        currentPage = currentPage
+    ) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            LessonHeader(
+                stringArrayResource(R.array.lesson_4_header_text)[currentPage.value],
+                Modifier
+                    .fillMaxWidth()
+                    .padding(dp_15),
+                TextAlign.Center
+            )
+
+            when (currentPage.value) {
+                0 -> AlertDialog()
+                1 -> BottomSheet()
+                2 -> AppRatingDialog()
+                3 -> LogoutDialogUI()
+            }
+        }
+    }
+}
