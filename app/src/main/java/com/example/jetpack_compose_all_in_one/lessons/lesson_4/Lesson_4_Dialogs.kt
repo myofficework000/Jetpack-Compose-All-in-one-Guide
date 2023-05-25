bc0f743aced379822ae1477be60ad0abe338d8e6
alex <mgpyaephyolay@gmail.com>
2023-05-25 16:48:43 -0500

    new bottom sheet

--- app/src/main/java/com/example/jetpack_compose_all_in_one/lessons/lesson_4/Lesson_4_Dialogs.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/lessons/lesson_4/Lesson_4_Dialogs.kt
@@ -1,20 +1,18 @@
 package com.example.jetpack_compose_all_in_one.lessons.lesson_4
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.text.style.TextAlign
 import com.example.jetpack_compose_all_in_one.R
 import com.example.jetpack_compose_all_in_one.ui.components.AlertDialog
+import com.example.jetpack_compose_all_in_one.ui.components.BottomSheet
 import com.example.jetpack_compose_all_in_one.ui.components.LessonHeader
 import com.example.jetpack_compose_all_in_one.ui.theme.dp_15
 import com.example.jetpack_compose_all_in_one.utils.LogicPager
@@ -47,7 +45,7 @@ private fun LessonContent() {
 
             when (currentPage.value) {
                 0 -> AlertDialog()
-                1 -> Box() {}
+                1 -> BottomSheet()
                 2 -> AppRatingDialog()
                 3 -> LogoutDialogUI()
             }
