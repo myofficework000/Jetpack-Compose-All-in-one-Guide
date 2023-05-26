1a91781e5f9ce32205c90a30403584a026dc0a7c
Luan Dang <luandang9697@gmail.com>
2023-05-25 22:58:08 -0500

    Implemented UI and logic for rating stars

--- app/src/main/java/com/example/jetpack_compose_all_in_one/lessons/lesson_4/AppRatingDialog.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/lessons/lesson_4/AppRatingDialog.kt
@@ -1,16 +1,17 @@
 package com.example.jetpack_compose_all_in_one.lessons.lesson_4
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.StarBorder
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Snackbar
@@ -40,6 +41,7 @@ import kotlinx.coroutines.delay
 fun AppRatingDialog() {
     var showPopUp by remember { mutableStateOf(true) }
     var showSnackBar by remember { mutableStateOf(false) }
+
     val onPopupDismissed = {
         showPopUp = false
         showSnackBar = true
@@ -66,37 +68,7 @@ fun AppRatingDialog() {
                         text = stringResource(R.string.rate_app_text),
                         textAlign = TextAlign.Center
                     )
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically
-                    ){
-                        Icon(
-                            imageVector = Icons.Filled.Star,
-                            contentDescription = R.string.rate_icon.toString(),
-                            tint = ShadowColor
-                        )
-                        Icon(
-                            imageVector = Icons.Filled.Star,
-                            contentDescription = R.string.rate_icon.toString(),
-                            tint = ShadowColor
-                        )
-                        Icon(
-                            imageVector = Icons.Filled.Star,
-                            contentDescription = R.string.rate_icon.toString(),
-                            tint = ShadowColor
-                        )
-                        Icon(
-                            imageVector = Icons.Filled.Star,
-                            contentDescription = R.string.rate_icon.toString(),
-                            tint = ShadowColor
-                        )
-                        Icon(
-                            imageVector = Icons.Filled.Star,
-                            contentDescription = R.string.rate_icon.toString(),
-                            tint = ShadowColor
-                        )
-                    }
+                    RatingStar()
                     TextField(
                         value = comment, 
                         onValueChange = {
@@ -144,5 +116,32 @@ fun AppRatingDialog() {
             }
         }
 
+    }
+}
+
+@Composable
+fun RatingStar() {
+    var rating by remember { mutableStateOf(0) }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        repeat(5){ index ->
+            val isFilled = rating > index
+            val starIcon = if (isFilled) Icons.Filled.Star else Icons.Outlined.StarBorder
+            Icon(
+                imageVector = starIcon,
+                contentDescription = R.string.rate_icon.toString(),
+                modifier = Modifier.clickable {
+                    rating = if (isFilled) {
+                        index
+                    } else {
+                        index + 1
+                    }
+                }
+            )
+        }
     }
 }
