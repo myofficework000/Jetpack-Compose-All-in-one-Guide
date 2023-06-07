b76e8d3e0a81fb4c36d35cfc3a63302a81dd3c94
alex <mgpyaephyolay@gmail.com>
2023-06-07 16:37:03 -0500

    two tests cases with email validation

--- app/src/androidTest/java/com/example/jetpack_compose_all_in_one/ui/views/sign_in_sign_up/RegistrationKtTest.kt
+++ app/src/androidTest/java/com/example/jetpack_compose_all_in_one/ui/views/sign_in_sign_up/RegistrationKtTest.kt
@@ -1,12 +1,16 @@
 package com.example.jetpack_compose_all_in_one.ui.views.sign_in_sign_up
 
-import androidx.activity.compose.setContent
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.clearCompositionErrors
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextClearance
+import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.jetpack_compose_all_in_one.lessons.lesson_9.CounterDisplay
+import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -40,9 +44,31 @@ class RegistrationKtTest{
         }
     }
 
-    /*fun verifyEmailValidation(){
-        composeTestRule.apply {
-            onNodeWithText("Email")
+    @Test
+    fun verifyEmailValidationWithValidEmail(){
+        val validEmail = "abcd@gmail.com"
+
+        composeTestRule.onNodeWithText("Email").apply {
+            performTextInput(validEmail)
+            assert(hasText(validEmail))
+            assertTrue(!isEmailValid.value)
+            assertTrue(emailErrMsg.value == "")
+            performTextClearance()
         }
-    }*/
+    }
+
+    @Test
+    fun verifyEmailValidationWithInvalidEmail(){
+        val invalidEmail = "invalidEmail"
+        val errorMsg = "Input proper email id"
+
+        composeTestRule.onNodeWithText("Email").apply {
+            performTextInput(invalidEmail)
+            assert(hasText(invalidEmail))
+            assertTrue(isEmailValid.value)
+            assertTrue(errorMsg == emailErrMsg.value)
+            performTextClearance()
+        }
+
+    }
 }
