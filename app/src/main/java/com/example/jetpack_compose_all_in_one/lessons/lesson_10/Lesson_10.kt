1e383e353b54d3adfc61511af2fd2cd0b6194523
cheetahmail007 <cheetahmail007@gmail.com>
2023-06-08 20:17:46 +0530

    Added lesson 10

---
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/lessons/lesson_10/Lesson_10.kt
@@ -0,0 +1,193 @@
+package com.example.jetpack_compose_all_in_one.lessons.lesson_10
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.material.icons.filled.StarBorder
+import androidx.compose.material.icons.outlined.CleanHands
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.LibraryMusic
+import androidx.compose.material.icons.outlined.ReadMore
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpack_compose_all_in_one.R
+import com.example.jetpack_compose_all_in_one.ui.components.LessonHeader
+import com.example.jetpack_compose_all_in_one.ui.components.LessonText2
+import com.example.jetpack_compose_all_in_one.ui.theme.twitterColor
+
+
+@Preview
+@Composable
+fun Lesson_10() {
+    LessonContent()
+}
+
+@Composable
+fun LessonContent() {
+    Text(text = "App Bars", modifier = Modifier.padding(8.dp))
+
+    TopAppBarsDemo()
+    BottomAppBarDemo()
+    NavigationBarDemo()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarsDemo() {
+    LessonHeader("Top App bar")
+
+    SmallTopAppBar(
+        title = { Text(text = "Home") },
+        navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = stringResource(id = R.string.back)
+                )
+            }
+        }
+    )
+
+    Spacer(modifier = Modifier.height(8.dp))
+
+    TopAppBar(
+        title = { Text(text = "Instagram") },
+        backgroundColor = MaterialTheme.colorScheme.surface,
+        contentColor = MaterialTheme.colorScheme.onSurface,
+        elevation = 8.dp,
+        navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_instagram),
+                    contentDescription = null
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(painter = painterResource(id = R.drawable.ic_send), contentDescription = null)
+            }
+        }
+    )
+
+    Spacer(modifier = Modifier.height(8.dp))
+
+    TopAppBar(
+        title = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_twitter),
+                contentDescription = null,
+                tint = twitterColor,
+                modifier = Modifier.fillMaxWidth()
+            )
+        },
+        backgroundColor = MaterialTheme.colorScheme.surface,
+        contentColor = MaterialTheme.colorScheme.onSurface,
+        elevation = 8.dp,
+        navigationIcon = {
+            Image(
+                painter = painterResource(id = R.drawable.baseline_menu_24),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(vertical = 4.dp, horizontal = 8.dp)
+                    .size(32.dp)
+                    .clip(CircleShape)
+            )
+        },
+        actions = {
+            Icon(
+                imageVector = Icons.Default.StarBorder,
+                contentDescription = null,
+                modifier = Modifier.padding(horizontal = 8.dp)
+            )
+        }
+    )
+    Spacer(modifier = Modifier.height(8.dp))
+}
+
+@Composable
+fun BottomAppBarDemo() {
+    Spacer(modifier = Modifier.height(16.dp))
+    LessonText2("Bottom app bars: Note bottom app bar support FAB cutouts when used with scafolds see demoUI crypto app")
+
+    BottomAppBar(
+        cutoutShape = CircleShape
+    ) {
+        IconButton(onClick = {}) {
+            Icon(imageVector = Icons.Default.MoreHoriz, contentDescription = null)
+        }
+        LessonText2("Bottom App Bar")
+    }
+}
+
+@Composable
+fun NavigationBarDemo() {
+    Spacer(modifier = Modifier.height(16.dp))
+    LessonText2("Bottom Navigation Bars")
+    val spotifyNavItemState = remember { mutableStateOf(false) }
+    BottomNavigation(backgroundColor = MaterialTheme.colorScheme.surface) {
+        BottomNavigationItem(
+            icon = { Icon(imageVector = Icons.Outlined.Home, contentDescription = null) },
+            selected = spotifyNavItemState.value,
+            onClick = { spotifyNavItemState.value = true },
+            label = { Text(text = stringResource(id = R.string.home)) },
+        )
+        BottomNavigationItem(
+            icon = { Icon(imageVector = Icons.Outlined.Search, contentDescription = null) },
+            selected = spotifyNavItemState.value,
+            onClick = { spotifyNavItemState.value = true },
+            label = { Text(text = stringResource(id = R.string.search)) }
+        )
+        BottomNavigationItem(
+            icon = { Icon(imageVector = Icons.Outlined.LibraryMusic, contentDescription = null) },
+            selected = spotifyNavItemState.value,
+            onClick = { spotifyNavItemState.value = true },
+            label = { Text(text = stringResource(id = R.string.music)) }
+        )
+    }
+
+    Spacer(modifier = Modifier.height(16.dp))
+
+    BottomNavigation {
+        BottomNavigationItem(
+            icon = { Icon(imageVector = Icons.Outlined.ReadMore, contentDescription = null) },
+            selected = spotifyNavItemState.value,
+            onClick = { spotifyNavItemState.value = true }
+        )
+        BottomNavigationItem(
+            icon = { Icon(imageVector = Icons.Outlined.Search, contentDescription = null) },
+            selected = spotifyNavItemState.value,
+            onClick = { spotifyNavItemState.value = true }
+        )
+        BottomNavigationItem(
+            icon = { Icon(imageVector = Icons.Outlined.CleanHands, contentDescription = null) },
+            selected = spotifyNavItemState.value,
+            onClick = { spotifyNavItemState.value = true }
+        )
+    }
+}
--- app/src/main/java/com/example/jetpack_compose_all_in_one/ui/theme/Color.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/ui/theme/Color.kt
@@ -35,6 +35,7 @@ val DefaultBubbleColor2 = Color(0xffE7FFDB)
 val SentMessageColor = Color(0xffE7FFDB)
 val ReceivedMessageColor = Color.White
 val DateColor = Color(0xffd4eaf4)
+
 //val BackgroundColor = Color(0xffefe8df)
 val BackgroundColor = Color(0xffFBE9E7)
 val ChatTextColor = Color.Black
@@ -69,3 +70,30 @@ val AqiGradient = Brush.horizontalGradient(
     0.6f to Color(0xFF7C09FF),
     0.7f to Color(0xFF520600)
 )
+
+val orange200 = Color(0xFFff7961)
+val orange500 = Color(0xFFf44336)
+val orange700 = Color(0xFFba000d)
+
+val teal200 = Color(0xff80deea)
+val twitterColor = Color(0xFF1DA1F2)
+val tiktokBlue = Color(0xFF69C9D0)
+val tiktokRed = Color(0xFFEE1D52)
+val tiktokBlack = Color(0xFF010101)
+val blue = Color(0xFF5851DB)
+
+val green200 = Color(0xffa5d6a7)
+val green500 = Color(0xff4caf50)
+val green700 = Color(0xff388e3c)
+
+val purple200 = Color(0xFFB39DDB)
+val purple = Color(0xFF833AB4)
+val purple700 = Color(0xFF512DA8)
+
+val orange = Color(0xFFF56040)
+val yellow = Color(0xFFFCAF45)
+val graySurface = Color(0xFF2A2A2A)
+val gradientGreenColors = listOf(green200, green500, green700)
+val gradientRedColors = listOf(orange, tiktokRed)
+val gradientBluePurple = listOf(blue, purple)
+val instagramGradient = listOf(blue, purple, orange, yellow)
