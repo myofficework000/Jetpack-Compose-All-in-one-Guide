5bbd3fe8df36deeb2148cfc1272b7971ff5c1460
cheetahmail007 <cheetahmail007@gmail.com>
2023-06-10 01:33:50 +0530

    old way with new way

--- app/src/main/java/com/example/jetpack_compose_all_in_one/ui/views/ComposeWithOldWayActivityExample.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/ui/views/ComposeWithOldWayActivityExample.kt
@@ -5,16 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import com.example.jetpack_compose_all_in_one.R
 import com.example.jetpack_compose_all_in_one.databinding.ActivityComposeWithOldWayExampleBinding
-import com.example.jetpack_compose_all_in_one.ui.theme.BackgroundColor
-import com.example.jetpack_compose_all_in_one.ui.theme.sp_25
+import com.example.jetpack_compose_all_in_one.ui.theme.orange
 
 class ComposeWithOldWayActivityExample : AppCompatActivity() {
     private lateinit var binding: ActivityComposeWithOldWayExampleBinding
@@ -35,12 +35,15 @@ fun ComposeContent() {
     Box(
         Modifier
             .fillMaxSize()
-            .background(BackgroundColor)
+            .background(orange)
     ) {
         Text(
-            text = stringResource(id = R.string.compose_text), fontSize = sp_25,
-            fontWeight = FontWeight.Bold,
-            color = Color.White
+            modifier = Modifier
+                .align(Alignment.Center),
+            text = stringResource(id = R.string.compose_text),
+            style = MaterialTheme.typography.h4,
+            color = MaterialTheme.colors.onBackground,
+            textAlign = TextAlign.Center
         )
     }
 }
