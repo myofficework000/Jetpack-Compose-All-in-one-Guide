3a939596c1f904fefa5ce7c2e1fb2eef9227047f
ABHISHEK PATHAK <51234843+myofficework000@users.noreply.github.com>
2023-06-12 02:53:03 +0530

    Updated Feature Flow
    Separated the flow demo in file as that will make the standard way of making features.
    
    Co-Authored-By: 1+1=2 <67162992+nahCauhsoJ@users.noreply.github.com>

--- app/src/main/java/com/example/jetpack_compose_all_in_one/features/flows_demo/StateFlowVsSharedFlow.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/features/flows_demo/StateFlowVsSharedFlow.kt
@@ -7,58 +7,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.jetpack_compose_all_in_one.R
-import com.example.jetpack_compose_all_in_one.ui.components.LessonHeader
 import com.example.jetpack_compose_all_in_one.ui.components.SimpleTextButton
 import com.example.jetpack_compose_all_in_one.ui.theme.dp_15
 import com.example.jetpack_compose_all_in_one.ui.theme.dp_30
-import com.example.jetpack_compose_all_in_one.utils.LogicPager
 
 @Composable
-fun FeatureFlowContent() {
-    val currentPage = rememberSaveable { mutableIntStateOf(0) }
-
-    LogicPager(
-        pageCount = 2,
-        currentPage = currentPage
-    ) {
-        Column(
-            Modifier
-                .fillMaxSize()
-                .padding(it)
-        ) {
-            LessonHeader(
-                stringArrayResource(R.array.feature_flow_example)[currentPage.value],
-                Modifier
-                    .fillMaxWidth()
-                    .padding(dp_15),
-                TextAlign.Center
-            )
-
-            when (currentPage.value) {
-                0 -> FlowDemoWithMultipleConsumer()
-                1 -> StateFlowVsSharedFlow()
-            }
-        }
-    }
-}
-
-@Composable
-private fun StateFlowVsSharedFlow() {
+fun StateFlowVsSharedFlow() {
     val flowsViewModel: FlowsViewModel = hiltViewModel()
     val state1 by flowsViewModel.stateFlow1.collectAsState()
     val state2 by flowsViewModel.sharedFlow1.collectAsState(0)
