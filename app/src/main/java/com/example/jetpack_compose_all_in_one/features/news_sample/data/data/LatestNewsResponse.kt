14093cfede760406241a20a1c7227ab7b48f8471
ABHISHEK PATHAK <51234843+myofficework000@users.noreply.github.com>
2023-06-15 00:32:27 +0530

    Added documentation for data class news

--- app/src/main/java/com/example/jetpack_compose_all_in_one/features/news_sample/data/data/LatestNewsResponse.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/features/news_sample/data/data/LatestNewsResponse.kt
@@ -1,5 +1,12 @@
 package com.example.jetpack_compose_all_in_one.features.news_sample.data.data
 
+/**
+ *
+Represents the response object for the latest news articles.
+@property news The list of news articles.
+@property page The current page number.
+@property status The status of the response.
+ */
 data class LatestNewsResponse(
     val news: List<News>,
     val page: Int,
