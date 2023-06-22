ad97edf9bc21018f44c578e213680ebbc97ff411
1+1=2 <hau.chan@jessup.edu>
2023-06-22 07:32:30 -0700

    Small fix
    - The web view of PagedLessonHeader is now scrollable
    - Added PagedLessonHeader with placeholder content to Lesson 1

--- app/src/main/java/com/example/jetpack_compose_all_in_one/utils/CommonUtils.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/utils/CommonUtils.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -182,7 +184,9 @@ fun PagedLessonHeader(
                             contentAlignment = Alignment.Center
                         ) { CircularProgressIndicator() }
 
-                        WebView(state = webViewState)
+                        Box(
+                            Modifier.verticalScroll(rememberScrollState())
+                        ) { WebView(state = webViewState) }
                     }else Text(this)
                 }
             }
