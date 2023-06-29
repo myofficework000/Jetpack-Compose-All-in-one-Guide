64598da900074421f6d10d9c441c66701ac2ccb4
1+1=2 <hau.chan@jessup.edu>
2023-06-29 15:05:41 -0700

    Small fix
    We missing a library is all.

--- app/src/main/java/com/example/jetpack_compose_all_in_one/android_architectures/clean_code_with_mvi_mvvm/data/remote/WeatherDto.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/android_architectures/clean_code_with_mvi_mvvm/data/remote/WeatherDto.kt
@@ -1,6 +1,5 @@
 package com.example.jetpack_compose_all_in_one.android_architectures.clean_code_with_mvi_mvvm.data.remote
 
-import com.example.jetpack_compose_all_in_one.android_architectures.clean_code_with_mvi_mvvm.data.remote.WeatherDataDto
 import com.squareup.moshi.Json
 
 data class WeatherDto(
