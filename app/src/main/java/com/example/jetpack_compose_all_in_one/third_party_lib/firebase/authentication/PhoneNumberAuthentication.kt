82ab3dd424bd6ad6dfaa5b7ad7cf38ff2c5bcdf3
DHNGL-ABHISHEKP\Abhishek P <myofficework000@gmail.com>
2023-11-02 23:27:11 +0300

    Added Firebase section in repo

---
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/third_party_lib/firebase/authentication/PhoneNumberAuthentication.kt
@@ -0,0 +1,15 @@
+package com.example.jetpack_compose_all_in_one.third_party_lib.firebase.authentication
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PhoneNumberAuthentication() {
+
+}
+
+@Preview
+@Composable
+fun PhoneNumberAuthenticationPreview() {
+
+}
