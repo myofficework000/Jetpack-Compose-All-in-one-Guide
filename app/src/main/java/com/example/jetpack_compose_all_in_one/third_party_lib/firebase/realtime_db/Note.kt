08c5a94b42f7a06ff6c47592d861b16bb948b84e
yao <yuanyao721@gmail.com>
2023-11-03 15:26:48 -0500

    add firebase realtime database project

---
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/third_party_lib/firebase/realtime_db/Note.kt
@@ -0,0 +1,7 @@
+package com.example.jetpack_compose_all_in_one.third_party_lib.firebase.realtime_db
+
+data class Note(
+    val title: String? = null,
+    val date: String? = null,
+    val content: String? = null
+)
