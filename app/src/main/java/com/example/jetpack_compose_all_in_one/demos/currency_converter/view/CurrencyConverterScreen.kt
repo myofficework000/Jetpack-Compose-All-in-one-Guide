d499d1a03c9893b2b236ef50ac2f0bd68d14a35b
DHNGL-ABHISHEKP\Abhishek P <myofficework000@gmail.com>
2023-11-13 23:00:38 +0300

    Added Currency converter screen template

---
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/demos/currency_converter/view/CurrencyConverterScreen.kt
@@ -0,0 +1,15 @@
+package com.example.jetpack_compose_all_in_one.demos.currency_converter.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CurrencyFromToScreen() {
+
+}
+
+@Preview
+@Composable
+fun CurrencyFromToScreenPrev() {
+
+}
---
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/third_party_lib/firebase/firestore_db/NotesDemo.kt
@@ -0,0 +1,8 @@
+package com.example.jetpack_compose_all_in_one.third_party_lib.firebase.firestore_db
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NotesDemo() {
+
+}
