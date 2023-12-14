1c12958e22503850f051468c5e928303ebc11663
YeJi Kim <ykim762@wisc.edu>
2023-12-14 11:43:22 -0700

    registration confirmpassword and compose testing

--- app/src/androidTest/java/com/example/jetpack_compose_all_in_one/ui/views/sign_in_sign_up/RegistrationKtTest.kt
+++ app/src/androidTest/java/com/example/jetpack_compose_all_in_one/ui/views/sign_in_sign_up/RegistrationKtTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextClearance
 import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -38,6 +39,7 @@ class RegistrationKtTest{
             onNodeWithText("Mobile Number").assertExists()
             onNodeWithText("Password").assertExists()
             onNodeWithText("Confirm Password").assertExists()
+            onNodeWithTag("Register button").assertExists()
         }
     }
 
@@ -66,6 +68,62 @@ class RegistrationKtTest{
             assertTrue(errorMsg == emailErrMsg.value)
             performTextClearance()
         }
+    }
 
+    @Test
+    fun verifyConfirmPasswordValidationWithValidPassword(){
+        val validConfirmPassword = ""
+        val errorMsg = ""
+
+        composeTestRule.onNodeWithText("Confirm Password").apply {
+            performTextInput(validConfirmPassword)
+            assert(hasText(validConfirmPassword))
+            assertFalse(isConfirmPasswordInvalid.value)
+            assertTrue(errorMsg == "")
+            performTextClearance()
+        }
+    }
+
+    @Test
+    fun verifyConfirmPasswordValidationWithInvalidPassword(){
+        val validConfirmPassword = "password"
+        val errorMsg = "Password is not matched"
+
+        composeTestRule.onNodeWithText("Confirm Password").apply {
+            performTextInput(validConfirmPassword)
+            assert(hasText(validConfirmPassword))
+            assertTrue(isConfirmPasswordInvalid.value)
+            assertTrue(errorMsg == "Password is not matched")
+            performTextClearance()
+        }
+    }
+
+    @Test
+    fun verifyButtonClick(){
+        val userName = "user"
+        val email = "abc@abc.com"
+        val mobile = "123-456-789"
+        val password = "password"
+        val confirmPassword = "password"
+
+        composeTestRule.onNodeWithText("Username")
+            .performTextInput(userName)
+
+        composeTestRule.onNodeWithText("Email")
+            .performTextInput(email)
+
+        composeTestRule.onNodeWithText("Mobile Number")
+            .performTextInput(mobile)
+
+        composeTestRule.onNodeWithText("Password")
+            .performTextInput(password)
+
+        composeTestRule.onNodeWithText("Confirm Password")
+            .performTextInput(confirmPassword)
+
+        composeTestRule.onNodeWithTag("Register button").performClick()
+
+        assertTrue(email.isNotEmpty())
+        assertTrue(password.isNotEmpty())
     }
 }
