71968103be531e229ab4f1d71082fbd09b3ae77d
hushiyan <hushiyan01@gmail.com>
2023-12-18 21:39:50 -0500

    added clickable logic to thread and unitest cases

--- app/src/androidTest/java/com/example/jetpack_compose_all_in_one/ThreadCommentSectionTest.kt
+++ app/src/androidTest/java/com/example/jetpack_compose_all_in_one/ThreadCommentSectionTest.kt
@@ -3,8 +3,10 @@ package com.example.jetpack_compose_all_in_one
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.jetpack_compose_all_in_one.demos.thread.ThreadCommentSection
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,18 +20,26 @@ class ThreadCommentSectionTest {
     @get:Rule
     val composableRule = createComposeRule()
 
-    @Test
-    fun uiComponentTest() {
+    @Before
+    fun setUp(){
         composableRule.setContent {
             ThreadCommentSection()
         }
-
         icon = composableRule.onNodeWithTag("comment_icon")
         firstOneMessageText = composableRule.onNodeWithTag("first_comment_static_text")
         sendMessageHereText = composableRule.onNodeWithTag("start_conversation_static_text")
+    }
 
+    @Test
+    fun uiComponentTest() {
         icon.assertExists()
         firstOneMessageText.assertExists()
         sendMessageHereText.assertExists()
+    }
+
+    @Test
+    fun startConversationTextClickableTest(){
+        sendMessageHereText.performClick()
+        sendMessageHereText.assertDoesNotExist()
     }
 }
--- app/src/main/java/com/example/jetpack_compose_all_in_one/demos/thread/ThreadCommentSection.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/demos/thread/ThreadCommentSection.kt
@@ -1,5 +1,8 @@
 package com.example.jetpack_compose_all_in_one.demos.thread
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,11 +10,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Message
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -26,6 +36,7 @@ import com.example.jetpack_compose_all_in_one.ui.theme.Purple40
 @Composable
 @Preview(showBackground = true)
 fun ThreadCommentSection() {
+    var conversationStarted by remember { mutableStateOf(false) }
     Column(Modifier.fillMaxWidth()) {
         Spacer(modifier = Modifier.height(200.dp))
         Icon(
@@ -40,19 +51,38 @@ fun ThreadCommentSection() {
         )
         Text(
             text = "Be the first to like & comment",
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
                 .padding(20.dp)
                 .testTag("first_comment_static_text"),
             style = TextStyle(fontSize = 30.sp, fontWeight = FontWeight.Bold),
             textAlign = TextAlign.Center
         )
-        Text(
-            text = "Start a Conversation",
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-                .padding(20.dp)
-                .testTag("start_conversation_static_text"),
-            style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Medium),
-            textAlign = TextAlign.Center
-        )
+        if(conversationStarted){
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(8.dp)) {
+                LazyColumn(){
+                    items(emptyList<String>()){
+
+                    }
+                }
+            }
+        }else{
+            Text(
+                text = "Start a Conversation",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(20.dp)
+                    .testTag("start_conversation_static_text")
+                    .clickable {
+                        conversationStarted = true
+                    },
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Medium),
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
