dc1736b3b60aa78059a7ec548afc0b130d6d333b
DHNGL-ABHISHEKP\Abhishek P <cheetahmail007@gmail.com>
2024-03-02 00:57:56 +0530

    Added documentation

--- app/src/main/java/com/example/jetpack_compose_all_in_one/application_components/content_provider/demo_contacts/Contact.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/application_components/content_provider/demo_contacts/Contact.kt
@@ -1,5 +1,12 @@
 package com.example.jetpack_compose_all_in_one.application_components.content_provider.demo_contacts
 
+/**
+ * Represents a contact with an ID, name, and phone number.
+ *
+ * @property id The unique identifier of the contact.
+ * @property name The name of the contact.
+ * @property phoneNumber The phone number of the contact.
+ */
 data class Contact(
     val id: String,
     val name: String,
