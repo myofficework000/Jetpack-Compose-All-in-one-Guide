8b9b480825dcf7de5e52eca77e538599ef0dc50b
DHNGL-ABHISHEKP\Abhishek P <cheetahmail007@gmail.com>
2024-03-14 03:00:47 +0530

    Updated Notification module

--- app/src/main/java/com/example/jetpack_compose_all_in_one/di/NotificationModule.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/di/NotificationModule.kt
@@ -13,27 +13,42 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+/**
+ * This module provides dependencies related to notifications, such as notification builder
+ * and notification manager.
+ */
 @Module
 @InstallIn(SingletonComponent::class)
 class NotificationModule {
 
+    /**
+     * Provides a NotificationCompat.Builder instance for building notifications.
+     * @param context Application context.
+     * @return NotificationCompat.Builder instance.
+     */
     @Singleton
     @Provides
     fun provideNotificationBuilder(
         @ApplicationContext context: Context
-    ):NotificationCompat.Builder{
-        return NotificationCompat.Builder(context,"Main Channel ID")
+    ): NotificationCompat.Builder {
+        return NotificationCompat.Builder(context, "Main Channel ID")
             .setContentTitle("Inspiration for today")
             .setContentText("Life is hard")
             .setSmallIcon(R.drawable.baseline_notifications_24)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
     }
 
+    /**
+     * Provides a NotificationManagerCompat instance for managing notifications.
+     * Also creates a notification channel if it doesn't exist.
+     * @param context Application context.
+     * @return NotificationManagerCompat instance.
+     */
     @Singleton
     @Provides
     fun provideNotificationManager(
         @ApplicationContext context: Context
-    ):NotificationManagerCompat{
+    ): NotificationManagerCompat {
         val notificationManager = NotificationManagerCompat.from(context)
         val channel = NotificationChannel(
             "Main Channel ID",
