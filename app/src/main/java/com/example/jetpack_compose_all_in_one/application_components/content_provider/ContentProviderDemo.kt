e7b181bd88187a8b9eb975ade7ec8efe604d5f94
DHNGL-ABHISHEKP\Abhishek P <cheetahmail007@gmail.com>
2024-03-17 02:30:34 +0530

    Added image fetching content provider

--- app/src/main/java/com/example/jetpack_compose_all_in_one/application_components/content_provider/ContentProviderDemo.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/application_components/content_provider/ContentProviderDemo.kt
@@ -21,19 +21,26 @@ import com.example.jetpack_compose_all_in_one.ui.components.LessonHeader
 import com.example.jetpack_compose_all_in_one.ui.theme.dp_15
 import com.example.jetpack_compose_all_in_one.utils.LogicPager
 
+/**
+ * Composable function for displaying the content related to ContentProvider.
+ */
 @Composable
 private fun ContentProviderContent() {
+    // Remembering the current page using mutableStateOf
     val currentPage = rememberSaveable { mutableStateOf(0) }
 
+    // LogicPager composable for handling page navigation
     LogicPager(
         pageCount = 3,
         currentPage = currentPage
     ) {
+        // Column composable to arrange content vertically
         Column(
             Modifier
                 .fillMaxSize()
                 .padding(it)
         ) {
+            // Displaying header based on the current page
             LessonHeader(
                 stringArrayResource(R.array.content_provider_header_text)[currentPage.value],
                 Modifier
@@ -42,6 +49,7 @@ private fun ContentProviderContent() {
                 TextAlign.Center
             )
 
+            // Displaying content based on the current page
             when (currentPage.value) {
                 0 -> ContactList()
                 1 -> ShowImages(9)
@@ -51,8 +59,12 @@ private fun ContentProviderContent() {
     }
 }
 
+/**
+ * Preview composable function for ContentProviderScreen.
+ */
 @Preview
 @Composable
 fun ContentProviderScreen() {
+    // Displaying ContentProviderContent in the preview
     ContentProviderContent()
 }
