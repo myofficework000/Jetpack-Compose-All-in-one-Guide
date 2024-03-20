378a478dfc86549abec0eaf5391404b5145cc386
DHNGL-ABHISHEKP\Abhishek P <cheetahmail007@gmail.com>
2024-03-21 02:01:08 +0530

    Added media3 video player demo

--- app/src/main/java/com/example/jetpack_compose_all_in_one/lessons/lesson_22/Media3ExoPlayer.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/lessons/lesson_22/Media3ExoPlayer.kt
@@ -14,33 +14,47 @@ import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 
+/**
+ * A composable function that creates a view for playing videos using ExoPlayer.
+ */
 @Composable
 fun ExoPlayerView() {
+    // URL of the video to be played
     val videoUrl = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+
+    // Get the current context
     val context = LocalContext.current
+
+    // Initialize ExoPlayer
     val exoPlayer = ExoPlayer.Builder(context).build()
 
+    // Create a media item from the video URL
     val mediaSource = remember(videoUrl) {
         MediaItem.fromUri(videoUrl)
     }
 
+    // Set up ExoPlayer with the media source
     LaunchedEffect(mediaSource) {
         exoPlayer.setMediaItem(mediaSource)
         exoPlayer.prepare()
     }
 
+    // Release ExoPlayer when the composable is disposed
     DisposableEffect(Unit) {
         onDispose {
             exoPlayer.release()
         }
     }
 
+    // Create an AndroidView for displaying the PlayerView
     AndroidView(
+        // Create the PlayerView and set the ExoPlayer
         factory = { ctx ->
             PlayerView(ctx).apply {
                 player = exoPlayer
             }
         },
+        // Set the layout parameters for the PlayerView
         modifier = Modifier
             .fillMaxWidth()
             .height(400.dp)
