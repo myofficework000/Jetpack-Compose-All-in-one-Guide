5d20b01fe1b07efab8d85fde23423d6d22bee99e
DHNGL-ABHISHEKP\Abhishek P <cheetahmail007@gmail.com>
2024-03-23 01:50:33 +0530

    Added currency converter demo

--- app/src/main/java/com/example/jetpack_compose_all_in_one/demos/currency_converter/data/api/CurrencyApiService.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/demos/currency_converter/data/api/CurrencyApiService.kt
@@ -4,8 +4,28 @@ import com.example.jetpack_compose_all_in_one.demos.currency_converter.data.dto.
 import retrofit2.Response
 import retrofit2.http.GET
 
+/**
+ * Interface for accessing a currency conversion API service.
+ * This interface defines methods to retrieve currency conversion rates.
+ */
 interface CurrencyApiService {
 
-    @GET("/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json")
-    suspend fun getEURToJPY() : Response<CurrencyResponse>
+    /**
+     * Companion object containing constant values for the CurrencyApiService interface.
+     */
+    companion object {
+        /**
+         * The base URL for the currency conversion API.
+         */
+        const val CURRENCY_API_URL = "/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json"
+    }
+
+    /**
+     * Retrieves the latest exchange rate from Euro (EUR) to Japanese Yen (JPY).
+     *
+     * @return A [Response] object containing the currency conversion response.
+     */
+    @GET(CURRENCY_API_URL)
+    suspend fun getEURToJPY(): Response<CurrencyResponse>
 }
+
