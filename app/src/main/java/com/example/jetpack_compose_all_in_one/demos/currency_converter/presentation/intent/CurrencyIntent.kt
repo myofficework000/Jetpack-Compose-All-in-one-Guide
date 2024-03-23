7ab0281df85cb7f64b378de80d9c7322e8ed84f8
DHNGL-ABHISHEKP\Abhishek P <cheetahmail007@gmail.com>
2024-03-24 02:31:55 +0530

    Added currency state UI

--- app/src/main/java/com/example/jetpack_compose_all_in_one/demos/currency_converter/presentation/intent/CurrencyIntent.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/demos/currency_converter/presentation/intent/CurrencyIntent.kt
@@ -1,5 +1,14 @@
 package com.example.jetpack_compose_all_in_one.demos.currency_converter.presentation.intent
 
+/**
+ * Sealed class representing intents related to currency operations.
+ */
 sealed class CurrencyIntent {
+
+    /**
+     * Intent representing a change in the input currency value.
+     *
+     * @property input The new input currency value.
+     */
     data class CurrencyInputChanged(val input: String): CurrencyIntent()
 }
--- app/src/main/java/com/example/jetpack_compose_all_in_one/demos/currency_converter/presentation/intent/CurrencyState.kt
+++ app/src/main/java/com/example/jetpack_compose_all_in_one/demos/currency_converter/presentation/intent/CurrencyState.kt
@@ -2,10 +2,32 @@ package com.example.jetpack_compose_all_in_one.demos.currency_converter.presenta
 
 import com.example.jetpack_compose_all_in_one.demos.currency_converter.data.dto.CurrencyResponse
 
+/**
+ * Sealed class representing various states related to currency operations.
+ */
 sealed class CurrencyState {
 
+    /**
+     * Represents the idle state, indicating no ongoing currency operation.
+     */
     object Idle: CurrencyState()
+
+    /**
+     * Represents the loading state, indicating an ongoing currency data retrieval operation.
+     */
     object Loading: CurrencyState()
+
+    /**
+     * Represents the success state, indicating successful retrieval of currency data.
+     *
+     * @property data The currency data retrieved successfully.
+     */
     data class Success(val data: CurrencyResponse) : CurrencyState()
+
+    /**
+     * Represents the error state, indicating a failure in currency data retrieval.
+     *
+     * @property error A string describing the error that occurred.
+     */
     data class Error(val error: String) : CurrencyState()
 }
